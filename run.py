471ac515bd80d400c361dc35b41ab00e6787afbc
damodamr <damie.juric@gmail.com>
2016-03-10 17:51:06 +0000

    Version 0.2

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import os
 
 app.secret_key = os.urandom(25)
 port = int(os.environ.get('PORT', 5000))
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port)
+#if __name__ == "__main__":
+app.run(host='0.0.0.0', port=port)
