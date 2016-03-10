1e5f4300a93967e7f6b6a1dcd3901f42fb94dcb0
damodamr <damie.juric@gmail.com>
2016-03-10 18:19:04 +0000

    Version 0.2

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from emborec import app
+from flask import Flask
 import os
 
+app = Flask(__name__)
 app.secret_key = os.urandom(25)
 port = int(os.environ.get('PORT', 5000))
-#if __name__ == "__main__":
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port)
